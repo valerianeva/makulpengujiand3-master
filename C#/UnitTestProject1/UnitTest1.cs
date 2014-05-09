cf667160135c83d9e2ae33e0f1c67790d3bfdcbd
laurasinar <laurasinarutami@gmail.com>
2014-05-09 11:57:53 +0700

    test

---
+++ C#/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCalculatorv1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void jumlah()
+        {
+            var test_object = new MainWindow();
+            double expected = 9;
+            double actual = test_object.Add(4, 5);
+            Assert.AreEqual<double>(expected, actual, "Maaf salah");
+        }
+
+        [TestMethod]
+        public void kurang()
+        {
+            var test_object = new MainWindow();
+            double expected = 2;
+            double actual = test_object.Substract(5, 3);
+            Assert.AreEqual<double>(expected, actual, "Maaf salah");
+        }
+
+        [TestMethod]
+        public void bagi()
+        {
+            var test_object = new MainWindow();
+            double expected = 3;
+            double actual = test_object.Divide(9, 3);
+            Assert.AreEqual<double>(expected, actual, "Maaf salah");
+        }
+
+        [TestMethod]
+        public void kali()
+        {
+            var test_object = new MainWindow();
+            double expected = 6;
+            double actual = test_object.Multiply(3, 2);
+            Assert.AreEqual<double>(expected, actual, "Maaf salah");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+makulpengujiand3
+================
+Tugas matakuliah pengujian D3:
+Berikut adalah source code yang berisi program kalkulator sederhana dalam bahasa C#.
+Silakan kalian membuat unit test untuk masing2 fungsi.
+Kemudian buat coded UI test untuk interface dari kalkulator tersebut.
+Silakan buat unit test dan coded ui testnya kemudian upload ke github.
+Berikan link github kalian ke saya pada pertemuan selanjutnya (kamis).
+
+Terima kasih,
+
+Hint: Jika kalian membuatnya selengkap mungkin, itu akan membantu kalian saat UAS nanti :)
